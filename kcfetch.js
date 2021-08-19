823414a7335a7487967d2f1b51cbefd7435b3533
Bea Steers <bea.steers@gmail.com>
2021-08-19 16:49:59 -0400

    API refactor - change withKeycloak to kcfetch.init

--- kcfetch.js
+++ kcfetch.js
@@ -6,22 +6,24 @@
     // https://www.keycloak.org/docs/latest/securing_apps/#javascript-adapter-reference
 
 
-    withKeycloak({
+    kcfetch.init({
         url: 'https://auth.myproject.com', // required
-        clientId: 'my-frontend-app', // required
-        realm: params.realm, // default: master
+        clientId: 'my-app', // required
+        realm: 'master', // default
 
         // options to pass to keycloak.init(). see js adapter reference above.
         initOptions: { onLoad: 'login-required' } // this is the default
         // if true (default), it will set `keycloak` and `kcfetch` as global variables.
         global: true,
-    }).then((kcfetch) => {
-        kcfetch(params)
     })
+
+    kcfetch('https://api.myproject.com/api/data').then(...)
 
 
 </script>
 */
+
+(() => {
 
 const insertScript = (path) => (
     new Promise((resolve, reject) => {
@@ -32,49 +34,61 @@ const insertScript = (path) => (
         document.head.appendChild(s);
     }));
 
-const makeKcfetch = (keycloak, defaultRefreshBuffer) => {
-    return (url, options={}, refreshBuffer) => (
-        new Promise((resolve, reject) => {
-            keycloak.updateToken(refreshBuffer || defaultRefreshBuffer || 10).then((refreshed) => {
-                fetch(url, {
-                    ...options, headers: {...options.headers, Authorization: 'Bearer ' + keycloak.token}
-                }).then(resolve).catch(reject);
-            }).catch((e) => { 
-                console.error('Failed to refresh token', e); 
-            });
-        })
-    )
-}
+const newKcfetch = ({  }={}) => {
+    const kcfetch = (url, options={}, refreshBuffer) => {
+        return new Promise((resolve, reject) => {
+            const initPromise = kcfetch._initPromise;
+            if(!initPromise) {
+                return reject("You must call kcfetch.init({ url, clientId }) to point it to your Keycloak instance.");
+            }
 
-const withKeycloak = ({ 
+            const request = () => (
+                kcfetch.keycloak.updateToken(refreshBuffer || 10).then((refreshed) => {
+                    fetch(url, {
+                        ...options, headers: {...options.headers, Authorization: 'Bearer ' + kcfetch.keycloak.token}
+                    }).then(resolve).catch(reject);
+                }).catch((e) => { 
+                    console.error('Failed to refresh token', e);
+                })
+            )
+    
+            const kc = kcfetch.keycloak;
+            return kc && kc.initialized ? request() : initPromise.then(() => request());
+        })
+    }
+    
+    kcfetch.init = ({ 
         // define your keycloak instance
         url, realm, clientId, 
         initOptions={ onLoad: 'login-required' }, // options to pass to keycloak.init
         refreshBuffer, // the minimum time to expiration before refreshing token. Default is 10
-        global=true, // set to false if you don't want to define keycloak and kcfetch as global variables
         urlAsIs=false, // don't append /auth to the keycloak url (because you have already)
-}) => new Promise((resolve, reject) => {
-    // prepare the url
-    if(!url) { throw "You must specify a url pointing to your keycloak server. e.g. 'https://auth.myproject.com'" }
-    url = urlAsIs ? url : url.replace(/\/$/, '') + '/auth';
+    }) => {
+        kcfetch._initPromise = new Promise((resolve, reject) => {
+            // prepare the url
+            if(!url) { throw "You must specify a url pointing to your keycloak server. e.g. 'https://auth.myproject.com'" }
+            url = urlAsIs ? url : url.replace(/\/$/, '') + '/auth';
+            kcfetch.refreshBuffer = refreshBuffer || kcfetch.refreshBuffer;
+            
+            const create = () => {
+                const keycloak = new Keycloak({ url, realm: realm || 'master', clientId });//patchKeycloak();
+                kcfetch.keycloak = keycloak;
+                keycloak.init(initOptions).then(() => resolve(kcfetch, keycloak)).catch(reject);
+            }
+            // if Keycloak is not already loaded
+            window.Keycloak ? create() : insertScript(url + '/js/keycloak.js').then(create).catch(reject);
+        })
+        return kcfetch._initPromise;
+    };
+
+    kcfetch.Factory = newKcfetch;
+    return kcfetch;
+}
+
+window.kcfetch = newKcfetch();
+
+})()
 
 
-    const create = () => {
-        const keycloak = new Keycloak({ url, realm: realm || 'master', clientId })
-        const kcfetch = makeKcfetch(keycloak, refreshBuffer);
-        if(global) {
-            window.keycloak = keycloak;
-            window.kcfetch = kcfetch;
-        }
-
-        keycloak.init(initOptions).then(() => {
-            if(global) { window.keycloakInitialized = true; }
-            resolve(kcfetch, keycloak);
-        }).catch(reject);
-    }
-    // if Keycloak is not already loaded
-    window.Keycloak ? create() : insertScript(url + '/js/keycloak.js').then(create).catch(reject);
-});
-
-const params = new URLSearchParams((new URL(document.currentScript.src)).search);
-window.keycloakReady = params.url ? withKeycloak(params) : null;
+// const params = new URLSearchParams((new URL(document.currentScript.src)).search);
+// if(params.url) kcfetch.init(params);
